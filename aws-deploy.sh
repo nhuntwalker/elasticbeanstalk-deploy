a7952699e57705b6d07d9b320a713576362a605f
nhuntwalker <nhuntwalker@Gmail.com>
2018-10-30 06:21:54 -0500

    Added some after-run prompts

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -11,8 +11,8 @@ aws rds create-db-instance\
     --db-instance-class $DB_INSTANCE_CLASS\
     --engine $DB_ENGINE\
     --master-username $DB_USERNAME\
-    --master-user-password $DB_PASSWORD
-    --vpc-security-group-ids $RDS_SG_VPC_ID
+    --master-user-password $DB_PASSWORD\
+    --vpc-security-group-ids $RDS_SG_VPC_ID > /dev/null
 
 RDS_SECURITY_GROUP_ID=$(aws rds describe-db-instances\
     --query 'DBInstances[?starts_with(DBName, `'$DB_INSTANCE_NAME'`)].VpcSecurityGroups[0].VpcSecurityGroupId'\
@@ -23,11 +23,11 @@ DB_HOST=$(aws rds describe-db-instances\
     --output text)
 
 CURRENT_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-aws ec2 authorize-security-group-ingress\
-    --group-id $RDS_SECURITY_GROUP_ID\
-    --protocol tcp\
-    --port $DB_PORT\
-    --cidr $CURRENT_IP/32
+# aws ec2 authorize-security-group-ingress\
+#     --group-id $RDS_SECURITY_GROUP_ID\
+#     --protocol tcp\
+#     --port $DB_PORT\
+#     --cidr $CURRENT_IP/32
 
 export DATABASE_URL="postgres://$DB_USERNAME:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_INSTANCE_NAME"
 
@@ -37,36 +37,40 @@ BEANSTALK_BUCKET_NAME=$(aws elasticbeanstalk create-storage-location --output te
 
 aws s3 cp $ARCHIVE_PATH s3://$BEANSTALK_BUCKET_NAME
 
-aws elasticbeanstalk create-application\
-    --application-name $APP_NAME
+aws elasticbeanstalk create-application-version\
+    --application-name $APP_NAME\
+    --version-label $APP_VERSION\
+    --source-bundle S3Bucket="$BEANSTALK_BUCKET_NAME",S3Key="$FILENAME"\
+    --auto-create-application > /dev/null
 
 aws elasticbeanstalk create-environment\
     --application-name $APP_NAME\
     --environment-name $APP_ENV_NAME\
     --version-label $APP_VERSION\
-    --solution-stack-name "$APP_STACK"
+    --solution-stack-name "$APP_STACK"\
+    --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=DATABASE_URL,Value=$DATABASE_URL > /dev/null
 
-aws elasticbeanstalk update-environment\
-    --environment-name $APP_ENV_NAME\
-    --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=DATABASE_URL,Value=$DATABASE_URL
-
-aws elasticbeanstalk create-application-version\
+echo 'Run the following *after* beanstalk is done (give it a few minutes):
+--------
+EBS_SECURITY_GROUP=$(aws elasticbeanstalk describe-configuration-settings\
     --application-name $APP_NAME\
-    --version-label $APP_VERSION\
-    --source-bundle S3Bucket="$BEANSTALK_BUCKET_NAME",S3Key="$FILENAME"
+    --environment-name $APP_ENV_NAME\
+    --query "ConfigurationSettings[0].OptionSettings[?contains(Namespace, `aws:autoscaling:launchconfiguration`) && contains(OptionName, `SecurityGroups`)].Value"\
+    --output text)
 
-EBS_SECURITY_GROUP=$(aws elasticbeanstalk describe-configuration-settings --application-name $APP_NAME --environment-name $APP_ENV_NAME --query 'ConfigurationSettings[0].OptionSettings[?contains(Namespace, `aws:autoscaling:launchconfiguration`) && contains(OptionName, `SecurityGroups`)].Value' --output text)
-
-# Update inbound database rules
+RDS_SECURITY_GROUP_ID=$(aws rds describe-db-instances\
+    --query "DBInstances[?starts_with(DBName, `$DB_INSTANCE_NAME`)].VpcSecurityGroups[0].VpcSecurityGroupId"\
+    --output text)
 
 aws ec2 authorize-security-group-ingress\
     --group-id $RDS_SECURITY_GROUP_ID\
     --protocol tcp\
     --port $DB_PORT\
-    --source-group $EBS_SECURITY_GROUP
+    --source-group $EBS_SECURITY_GROUP > /dev/null
 
 aws ec2 authorize-security-group-ingress\
     --group-name $EBS_SECURITY_GROUP\
     --protocol tcp\
     --port $DB_PORT\
-    --source-group $RDS_SECURITY_GROUP_ID
+    --source-group $RDS_SECURITY_GROUP_ID > /dev/null
+'
--- aws-teardown.sh
+++ aws-teardown.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 set -e
 
-# RDS variables
-export DB_INSTANCE_NAME='tutorial_db'
-export DB_INSTANCE_ID='tutorial-db-instance'
-export DB_INSTANCE_CLASS='db.t2.micro'
-export DB_ENGINE='postgres'
-export DB_USERNAME='tutorialuser'
-export DB_PASSWORD='potatosalad'
-export DB_PORT=5432
-
-# EBS variables
-export APP_NAME='flask-expenses'
-export APP_ENV_NAME='flask-expenses-env'
-export APP_STACK="64bit Amazon Linux 2018.03 v2.7.4 running Python 3.6"
-export APP_VERSION="latest"
-
-# Gather security groups
+# Gather secondary artifacts
 RDS_SECURITY_GROUP_ID=$(aws rds describe-db-instances\
     --query 'DBInstances[?starts_with(DBName, `'$DB_INSTANCE_NAME'`)].VpcSecurityGroups[0].VpcSecurityGroupId'\
     --output text)
@@ -27,6 +12,14 @@ EBS_SECURITY_GROUP=$(aws elasticbeanstalk describe-configuration-settings\
     --query 'ConfigurationSettings[0].OptionSettings[?contains(Namespace, `aws:autoscaling:launchconfiguration`) && contains(OptionName, `SecurityGroups`)].Value'\
     --output text)
 
+BEANSTALK_BUCKET_NAME=$(aws elasticbeanstalk create-storage-location --output text)
+
+aws ec2 revoke-security-group-ingress\
+    --group-id $RDS_SECURITY_GROUP_ID\
+    --protocol tcp\
+    --port $DB_PORT\
+    --source-group $EBS_SECURITY_GROUP
+
 aws rds delete-db-instance\
     --db-instance-identifier $DB_INSTANCE_ID\
     --skip-final-snapshot
@@ -34,3 +27,6 @@ aws rds delete-db-instance\
 aws elasticbeanstalk delete-application\
     --application-name $APP_NAME\
     --terminate-env-by-force
+
+
+
